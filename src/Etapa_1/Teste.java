361b5e2af06aec7e3f9851d0a907f8df36094004
LeoBertol <leonardo_bertol@estudante.sc.senai.br>
2019-08-11 14:43:29 -0300

    Remoção da classe Play e correção da classe Teste

--- src/Etapa_1/Teste.java
+++ src/Etapa_1/Teste.java
@@ -4,11 +4,11 @@ public class Teste {
 	
 	public static void main(String args[]) {
 		
-		Play teste = new Play();
+		AIFFSuperPlayer teste = new AIFFSuperPlayer();
 		
 		String musica = "Hino da AAASC.mp3";
 		
-		teste.Abrir(musica);
+		teste.abrir(musica);
 		
 		
 	}
