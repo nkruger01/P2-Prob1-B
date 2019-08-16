ba0ebed42c40812dd7ea9c096d20ea4ff8ad9f58
LeoBertol <leonardo_bertol@estudante.sc.senai.br>
2019-08-16 20:30:03 -0300

    Inserção das classes na etapa 02

--- Etapa 2/Etapa_2/Teste.java
+++ Etapa 2/Etapa_2/Teste.java
@@ -1,5 +1,17 @@
 package Etapa_2;
 
 public class Teste {
+	
+	public static void main(String args[]) {
+		
+		AIFFSuperPlayer teste = new AIFFSuperPlayer();
+		
+		String musica = "Hino da AAASC.mp3";
+		
+		teste.abrir(musica);
+		
+		
+		
+	}
 
 }
